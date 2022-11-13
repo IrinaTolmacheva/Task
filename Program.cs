7c0fa9b6d423d5fd871f4f53535324ec49f84ad7
IrinaTolmacheva <tolmahevikml@mail.ru>
2022-11-13 12:56:29 +0300

    Решение задачи добавлено

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+void Process(string[] array)
+{
+
+    Console.WriteLine("Input:");
+    foreach (string val in array)
+    {
+        Console.WriteLine(val);
+    }
+
+    Console.WriteLine("Processing...");
+    array = array.Where(val => val.Length <= 3).ToArray();
+    Console.WriteLine("Result:");
+
+    foreach (string val in array)
+    {
+        Console.WriteLine(val);
+    }
+}
+
+
+string[] array = new string[] { "Hello", "2", "world", ":-)", "qwewqeqweq" };
+
+Process(array);
+
+array = new string[] { "1234", "1567", "-2", "computer science" };
+
+Process(array);
